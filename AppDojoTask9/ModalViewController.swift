c2b82afe077c20220cb15321b0c1a6033d386247
Nao-RandD <nao.r.and.d@gmail.com>
2021-04-14 21:21:36 +0900

    [add] Add NavigationControler and base UI.

---
+++ AppDojoTask9/ModalViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ModalViewController.swift
+//  AppDojoTask9
+//
+//  Created by Naoyuki Kan on 2021/04/14.
+//
+
+import UIKit
+
+class ModalViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+}
